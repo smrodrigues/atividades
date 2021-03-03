75e0aed61d50fbfe1964803a21fcada2a6c577af
smrodrigues <sabrinamararodrigues31@gmail.com>
2021-03-03 14:06:22 -0300

    atividade 5 - adicionando novas classes e construindo diagrama UML

--- back-end/models.py
+++ back-end/models.py
@@ -19,3 +19,50 @@ class Paciente(db.Model): #herdando da classe da model
             "idade": self.idade,
             "email": self.email
         }
+
+class Medico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    idade = db.Column(db.Integer)
+    email = db.Column(db.String(254))
+    tipo_sanguineo = db.Column(db.String(3))
+
+
+    def __str__(self):
+        return f'{self.id}. {self.nome}, {self.idade}, {self.email}, {self.tipo_sanguineo}'
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "idade": self.idade,
+            "email": self.email,
+            "tipo_sanguineo ": self.tipo_sanguineo 
+        }
+
+
+class Receita(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    codigo = db.Column(db.String(254))
+    id_paciente = db.Column(db.Integer, db.ForeignKey(Paciente.id), nullable=False)
+    paciente = db.relationship("Paciente")
+    id_medico = db.Column(db.Integer, db.ForeignKey(Medico.id), nullable=False)
+    medico = db.relationship("Medico")
+
+
+    def __str__(self):
+        return f'''{self.id}. {self.codigo}
+            {self.id_paciente} - {self.paciente.json()};
+            {self.id_medico} - {self.medico.json()}'''
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "codigo": self.codigo,
+            "id_paciente": self.id_paciente,
+            "paciente": self.paciente.json(),
+            "id_medico": self.id_medico,
+            "medico": self.medico.json()
+        }
--- back-end/test.py
+++ back-end/test.py
@@ -11,8 +11,21 @@ if __name__ == "__main__":
     p2 = Paciente(nome="Riad", idade=78, email="riadteacher@gmail.com")
     p3 = Paciente(nome="Ricardo", idade=22, email="rick@gmail.com")
 
+    m1 = Medico(nome="Marcelo", idade=32, email="doutormarcelo@gmail.com", tipo_sanguineo="AB+")
+    m2 = Medico(nome="Marcela", idade=23, email="doutoramarcela@gmail.com", tipo_sanguineo="AB-")
+
+    r1 = Receita(codigo="7362", id_paciente=1, paciente=p1, id_medico=1, medico=m1)
+    r2 = Receita(codigo="2637", id_paciente=2, paciente=p2, id_medico=2, medico=m2)
     db.session.add(p1)
     db.session.add(p2)
     db.session.add(p3)
+    db.session.add(m1)
+    db.session.add(m2)
+    db.session.add(r1)
+    db.session.add(r2)
     #adicionando models no banco
     db.session.commit() #commit para salvar as alterações
+
+    print(m1)
+    print(m1.json())
+    print(r1.json())
